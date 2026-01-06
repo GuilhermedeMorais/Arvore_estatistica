394c62833aad999e95f64ebe9484e20c93e588a0
Guilherme A <101284211+GuilhermedeMorais@users.noreply.github.com>
2026-01-06 16:56:16 -0300

    Update tree.js

--- tree.js
+++ tree.js
@@ -50,8 +50,9 @@ Para utilizar a árvore decisória alguns conceitos serão importantes, os termo
 Porém, é indicado que, ao utilizar o app você leia o manual que será linkado abaixo, pelo manual você poderá acessar explicações mais detalhadas acerca dos conceitos necessários para a utilização deste app.
 Esperamos que você faça bom proveito da nossa ferramenta.
 <br/>
-</a href="https://www.canva.com/design/DAG9kDdFhJw/w2Pa-tX1whvOsWDDuiJTNw/view?utm_content=DAG9kDdFhJw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h9a907434ba"
-   target="_blank">
+<a href="https://www.canva.com/design/DAG9kDdFhJw/w2Pa-tX1whvOsWDDuiJTNw/view?utm_content=DAG9kDdFhJw&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h9a907434ba"
+   target="_blank"
+   rel="noopener noreferrer">
   Manual da Árvore Decisória
 </a>
 <br/>
@@ -352,6 +353,7 @@ document.getElementById("back-button").onclick = () => {
 };
 
 displayNode("start");
+
 
 
 
